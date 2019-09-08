4050caa83b0ecb381f8391eee02745a58d93654f
Byron Vickers <byron.vickers@gmail.com>
2019-09-08 15:49:51 +1000

    Created client repo for gh-pages

---
+++ leaflet.js
@@ -0,0 +1,28 @@
+var map = L.map('map', { zoomControl:false }).setView([-35.4735, 149.0124], 5);
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	minZoom: 3,
+	maxZoom: 12,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	minZoom: 3,
+	maxZoom: 13
+});
+
+Esri_WorldGrayCanvas.addTo(map)
+
+fetch('https://3jaz6s2dul.execute-api.ap-southeast-2.amazonaws.com/dev/trams')
+.then(res => res.json())
+.then(data => {
+        data.forEach((item) => {
+          console.log(item.stopInfo.stop_name)
+
+          L.marker([item.stopInfo.stop_lat, item.stopInfo.stop_lon],
+           { title: item.stopInfo.stop_name })
+           .addTo(map)
+        })
+	})
+
